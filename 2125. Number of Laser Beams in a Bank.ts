ce5b3d7fd8af431cba15f4880283391a1f28eafc
youssefKetata <yusufketata5@gmail.com>
2025-10-27 18:19:14 +0100

    2125. Number of Laser Beams in a Bank

--- 2125. Number of Laser Beams in a Bank.ts
+++ 2125. Number of Laser Beams in a Bank.ts
@@ -1,19 +1,32 @@
 function numberOfBeams(bank: string[]): number {
-  const counts = bank.map((row) => {
-    const matches = row.match(/1/g);
-    return matches ? matches.length : 0;
-  });
-
-  let total = 0;
-  let prev = 0;
-  for (const count of counts) {
-    if (count === 0) continue;
-    if (prev > 0) total += prev * count;
-    prev = count;
+  let i1 = 0;
+  let i2 = 1;
+  let sum = 0;
+  while (i2 < bank.length) {
+    let row1 = bank[i1];
+    let row2 = bank[i2];
+    let count1 = 0;
+    let count2 = 0;
+    for (let k = 0; k < row1.length; k++) {
+      count1 += row1[k] == '1' ? 1 : 0;
+      count2 += row2[k] == '1' ? 1 : 0;
+    }
+    if (count2 == 0) {
+      i2++;
+      continue;
+    }
+    if (count1 == 0) {
+      i1 = i2;
+      i2++;
+    }
+    if (count1 > 0 && count2 > 0) {
+      sum += count1 * count2;
+      i1 = i2;
+      i2++;
+    }
   }
 
-  return total;
+  return sum;
 }
-
 let bank = ['000', '111', '000'];
 console.log(numberOfBeams(bank));
