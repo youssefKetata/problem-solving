9926328edbf2eead606cee99aa2e5497ecec7b1a
youssefKetata <yusufketata5@gmail.com>
2025-10-31 23:02:58 +0100

    3289. The Two Sneaky Numbers of Digitville

--- 3289. The Two Sneaky Numbers of Digitville.ts
+++ 3289. The Two Sneaky Numbers of Digitville.ts
@@ -12,5 +12,45 @@ function getSneakyNumbers(nums: number[]): number[] {
   }
   return sneaky;
 }
+
+// solution 2
+function getSneakyNumbers2(nums: number[]): number[] {
+  const n = nums.length - 2;
+  const res: number[] = [];
+  for (let i = 0; i < nums.length && res.length < 2; i++) {
+    const val = nums[i] % n;
+    if (nums[val] >= n) {
+      if (!res.includes(val)) res.push(val);
+    } else {
+      nums[val] += n;
+    }
+  }
+  return res;
+}
+
+function getSneakyNumbers3(nums: number[]): number[] {
+  const n = nums.length - 2;
+  const res: number[] = [];
+  for (let i = 0; i < nums.length && res.length < 2; i++) {
+    const val = nums[i] % n;
+    if (nums[val] >= n) {
+      if (!res.includes(val)) res.push(val);
+    } else {
+      nums[val] += n;
+    }
+  }
+  return res;
+}
+function getSneakyNumbers4(nums: number[]): number[] {
+  const map = new Map<number, number>();
+  const res: number[] = [];
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+    if (map.get(num) === 2) {
+      res.push(num);
+    }
+  }
+  return res;
+}
 const inputNumbers: number[] = [0, 1, 1, 0];
 console.log(getSneakyNumbers(inputNumbers));
