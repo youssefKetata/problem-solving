58317e68aa2b053e58eff0aa33d8872044541cbb
youssefKetata <yusufketata5@gmail.com>
2025-11-10 11:43:33 +0100

    3542. Minimum Operations to Convert All Elements to Zero

---
+++ daily/3542. Minimum Operations to Convert All Elements to Zero.ts
@@ -0,0 +1,15 @@
+function minOperations(nums: number[]): number {
+  const s = [];
+  let res = 0;
+  for (const a of nums) {
+    while (s.length && s[s.length - 1] > a) {
+      s.pop();
+    }
+    if (a === 0) continue;
+    if (!s.length || s[s.length - 1] < a) {
+      res++;
+      s.push(a);
+    }
+  }
+  return res;
+}
